53c6b9b87d14c897fe115913a642604931fac6f3
Sylvester Mbah <sylvestermbah90@gmail.com>
2023-05-09 03:40:27 -0700

    Create a module with functions that set and reset the field when it is being invoked/called

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ const Player = (player) => {
     return { getPlayer };
 }
 
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", ""];
+
+    const setField = (index, player) => {
+        if (index > board.length) return;
+        board[index] = player;
+    }
+
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    }
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = '';
+        }
+    }
+
+    return { setField, getField, reset };
+})();
+
